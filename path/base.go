9a65f68dbff60288388821459322262cbfec174b
menxqk <menxqk@gmail.com>
2022-11-06 19:28:58 -0300

    path

---
+++ path/base.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"path"
+)
+
+func main() {
+	fmt.Println(path.Base("/a/b"))
+	fmt.Println(path.Base("/"))
+	fmt.Println(path.Base(""))
+
+}
---
+++ path/filepath/join.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"path/filepath"
+)
+
+func main() {
+	fmt.Println("On Unix:")
+	fmt.Println(filepath.Join("a", "b", "c"))
+	fmt.Println(filepath.Join("a", "b/c"))
+	fmt.Println(filepath.Join("a/b", "c"))
+	fmt.Println(filepath.Join("a/b", "/c"))
+
+	fmt.Println(filepath.Join("a/b", "../../../xyz"))
+}
