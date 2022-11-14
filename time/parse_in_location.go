d89e3ab629b7b3ff5fe7df6d40e9f1010bd94f00
menxqk <menxqk@gmail.com>
2022-11-13 22:28:26 -0300

    time

---
+++ time/parse_in_location.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	loc, _ := time.LoadLocation("Europe/Berlin")
+
+	// This will look for th ename CEST in the Europe/Berlin time zone.
+	const longForm = "Jan 2, 2006 at 3:04pm (MST)"
+	t, _ := time.ParseInLocation(longForm, "Jul 9, 2012 at 5:02am (CEST)", loc)
+	fmt.Println(t)
+
+	// Note: without explicit zone, return time in given location.
+	const shortForm = "2006-Jan-02"
+	t, _ = time.ParseInLocation(shortForm, "2012-Jul-09", loc)
+	fmt.Println(t)
+}
